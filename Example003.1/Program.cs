8eb54241426ccf389333aa5d3ab2df42e0c7c664
Valeriy <walera.ru11@mail.ru>
2022-07-07 15:35:18 +0300

    add Example003.1 Random

---
+++ Example003.1/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(-10, 15); // 1,2,3,4...14
+Console.Write("NumberA = ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(-5, 7);
+Console.Write("NumberB = ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
